e3514cc41854a6061a5b9d42dbb186c97825ee42
XV <voxxys@gmail.com>
2018-09-25 12:27:23 +0300

    files for easy experiment start added

---
+++ pynfb/run_center_out_van_10s_gen_test.py
@@ -0,0 +1,16 @@
+
+
+import sys
+from PyQt5 import QtGui
+from pynfb.experiment import Experiment
+from pynfb.io.xml_ import xml_file_to_params
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    experiment = Experiment(app, xml_file_to_params('set_center_out_van_10s_gen_test.xml'))
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
